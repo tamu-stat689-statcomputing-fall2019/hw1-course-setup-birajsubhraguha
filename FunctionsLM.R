b1ede210f3c7604f8c4b55ddf93e828468ff3c9f
birajsubhraguha <49694199+birajsubhraguha@users.noreply.github.com>
2019-08-30 05:24:45 -0500

    FunctionsLM file filled in with 3 functions

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -5,9 +5,17 @@
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
-  
+  Y=numeric(0)
+  for(i in 1:n){
+    
+    # calculating means for each data point
+    u=beta%*%X[i,]
+    
+    # generating normal Y
+    Y=c(Y,rnorm(1, mean = u,sd = sigma))
   # Return Y
   return(Y)
+  }
 }
 
 # Calculate beta_LS - least-squares solution, do not use lm function
@@ -15,14 +23,16 @@ generateY <- function(X, beta, sigma, seed = 5832652){
 # Y -response
 calculateBeta <- function(X, Y){
   # Calculate beta_LS
-  
+  # Use well known OLS formula
+  X1=t(X)
+  beta_LS=solve(X1%*%X,X1%*%Y)
   # Return beta
   return(beta_LS)
 }
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
-  
+  MSE=crossprod(beta-beta_LS)
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
 }
